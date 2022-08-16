f60fef4dd2d37d0316361a23e1caf6b18d888996
Sekaia <gillotte.bm@gmail.com>
2022-08-16 18:10:58 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+import sys
+from sprites import *
+from config import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont('consolas', 32)
+        self.running = True
+
+    def new(self):
+        # New game starts
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.player = Player(self, 1, 2)
+
+    def events(self):
+
+    def update(self):
+
+    def draw(self):
+
+    def main(self):
+        # game loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    def game_over(self):
+
+    def intro_screen(self):
